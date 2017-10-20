f06879fc3d4f9d0f20d94eb939f2a91a214a2aca
Roy Hooper <rhooper@toybox.ca>
2017-10-19 22:05:23 -0400

    rework setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='encryptme_stats',
-      version='0.9.3',
+      version='0.9.4',
       description='Encrypt.me Statistics gatherer',
       author='Roy Hooper',
       author_email='rhooper@toybox.ca',
@@ -11,5 +11,14 @@ setup(name='encryptme_stats',
       entry_points={
               'console_scripts': [
                   'encryptme-stats = encryptme_stats:main',
-              ]}
+              ]},
+      install_requires=[
+          'netifaces==0.10.6',
+          'psutil==5.4.0',
+          'uptime==3.0.1',
+          'proc==0.14',
+          'docker==2.5.1',
+          'schedule==0.4.3',
+          'requests==2.18.4',
+      ],
      )
